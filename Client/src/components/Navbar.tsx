df40c3902cdc3a6e3b310219a17718f547e80b87
Md Farid <mdfarid.0118@gmail.com>
2026-01-25 22:58:36 +0530

    empty details

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -52,7 +52,7 @@ const Navbar = () => {
           ) : (
             <>
             <button className='bg-white/10 px-5 py-1.5 text-xs sm:text-sm border text-gray-200 rounded-full'>
-            Credit : <span className='text-indigo-300'>{credits}</span>
+            Credits: <span className='text-indigo-300'>{credits}</span>
             </button>
             <UserButton size='icon'/>
             </>
